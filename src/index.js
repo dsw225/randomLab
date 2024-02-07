846d4191c55ec99727593db4f0507d6949123fe4
dsw225 <89610923+dsw225@users.noreply.github.com>
2024-02-07 10:38:25 -0500

    update commitx3

--- src/index.js
+++ src/index.js
@@ -6,10 +6,22 @@ const characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
 /*
 Random 6 Alphanumeric hex method
 */
-function generateColor(length) {
-    let result = ' ';
+
+function generateColor() {
+    let result = '#';
     for ( let i = 0; i < 6; i++ ) {
         result += characters.charAt(Math.floor(Math.random() * 6));
     }
+    
     return result;
+}
+
+function boxColor() {
+    let color1 =generateColor();
+    let color2 =generateColor();
+    let color3 =generateColor();
+
+    document.getElementById("color-box1").style.backgroundColor = color1;
+    document.getElementById("color-box2").style.backgroundColor = color2;
+    document.getElementById("color-box3").style.backgroundColor = color3;
 }
