15b3def074f45633cced9d5396243e6b06675a63
dsw225 <89610923+dsw225@users.noreply.github.com>
2024-02-07 10:08:57 -0500

    update commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
+/*
+All possible characters allowed in hex
+*/
 const characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
 
+/*
+Random 6 Alphanumeric hex method
+*/
 function randomColor(length) {
     let result = ' ';
     for ( let i = 0; i < 6; i++ ) {
